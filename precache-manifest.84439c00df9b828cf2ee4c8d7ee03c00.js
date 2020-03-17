e6be5f0db775fd535099fa27ae2d24a2c1fd4f92
Marcio <marcio@Marcios-iMac.modem>
2020-03-17 23:06:37 +1100

    Updates

---
+++ precache-manifest.84439c00df9b828cf2ee4c8d7ee03c00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b0eefe1313a04b0980eb7ed621c286",
+    "url": "/inspector/index.html"
+  },
+  {
+    "revision": "eef2243d96dbff4c8ec7",
+    "url": "/inspector/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6ab0f0bf92989da5a27c",
+    "url": "/inspector/static/js/2.eb788a5e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/inspector/static/js/2.eb788a5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef2243d96dbff4c8ec7",
+    "url": "/inspector/static/js/main.0f6d38b1.chunk.js"
+  },
+  {
+    "revision": "d1290465e93604e8c903",
+    "url": "/inspector/static/js/runtime-main.a1e02e25.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/inspector/static/media/logo.5d5d9eef.svg"
+  }
+]);
